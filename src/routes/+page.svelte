1da6445af8d2e9be84a2c6709037e8624cac2690
reikrom <reikrom.lv@gmail.com>
2024-01-05 02:43:18 +0200

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script >
+    import '../lib/blockrain/blockrain.css'
+     const blockrain = () => import('$lib/blockrain/blockrain.jquery.min.js')
+    
+    // window.Jquery = 'hello'
+
+    // console.log(`%cRei: window`, 'background: #a53d63; color: white; font-size: 18px', window)
+    const click = async () => {
+        await blockrain();
+        window.$('.game').blockrain()
+    }
+</script>
+
+<div class="game" style="width:250px; height:500px; border: 1px solid red;"></div>
+<button on:click={click}>Play</button>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
